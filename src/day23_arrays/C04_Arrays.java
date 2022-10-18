d907bb265a74a19ae8ec7755ac175dec469e11d9
Jr. Full Stack Java Developper <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2022-10-19 01:49:06 +0300

    19.10.22

--- src/day23_arrays/C04_Arrays.java
+++ src/day23_arrays/C04_Arrays.java
@@ -9,7 +9,7 @@ public class C04_Arrays {
 
         int [] arr = {3,-3,5,-7,-6,1,-2};
 
-        int toplam =0;
+        System.out.println(pozitifElementleriTopla(arr)); // 9
 
         /*
         for (int i =0;i< arr.length; i++){
@@ -20,6 +20,18 @@ public class C04_Arrays {
         System.out.println(toplam);
         */
 
+        }
+
+    public static int pozitifElementleriTopla(int[] arr){
+
+        int toplam=0;
+
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i]>0){
+                toplam += arr[i];
+            }
+        }
+        return toplam;
         }
     }
 
