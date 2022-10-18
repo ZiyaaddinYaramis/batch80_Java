289325cf3588c4d48f0d8965e693871954dad302
Jr. Full Stack Java Developper <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2022-10-19 01:26:06 +0300

    19.10.22

--- src/day13_methodCreation/C03_MethodCreation.java
+++ src/day13_methodCreation/C03_MethodCreation.java
@@ -2,4 +2,19 @@ package day13_methodCreation;
 
 public class C03_MethodCreation {
 
-}
+    public static void main(String[] args) {
+        // verilen 3 double sayinin ortalamasini yazdiran  bir method yazin
+        // main method icinden cagirarak method'u calistirin
+
+
+        ortalama(80,60,55);
+
+    } // Mm sonu
+
+
+    public static void ortalama(double sayi1,double sayi2,double sayi3){
+
+        System.out.println("Girdiginiz iki sayinin ortalamasi : " + (sayi1+sayi2+sayi3)/3);
+
+    }
+} // C sonu
