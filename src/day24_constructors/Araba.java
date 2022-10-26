f26deb1ae576371fb2e911f84b95e848808efbaa
Jr. Full Stack Java Developper <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2022-10-26 21:51:54 +0300

    26.10.2022

--- src/day24_constructors/Araba.java
+++ src/day24_constructors/Araba.java
@@ -2,31 +2,53 @@ package day24_constructors;
 
 public class Araba {
 
-        String marka = "Marka belirtilmedi";
-        String model = "Model belirtilmedi";
-        String yakit;
-        int yil;
-        int fiyat;
+
+    String marka="Marka belirtilmedi";
+    String model="Model belirtilmedi";
+    String yakit;
+    int yil;
+    int fiyat;
 
 
-        public Araba(String mrk, String mdl){
-        }
+    public Araba(String marka, String model, String yakit, int yil, int fiyat) {
+        this.marka = marka;
+        this.model = model;
+        this.yakit = yakit;
+        this.yil = yil;
+        this.fiyat = fiyat;
+    }
 
 
-        public Araba() {
-        }
+
+
+    public Araba (String mrk,String mdl){
+        marka=mrk;
+        model=mdl;
+    }
+
+
+
+    public Araba(String marka, String model, int fiyat) {
+        this.marka = marka;
+        this.model = model;
+        this.fiyat = fiyat;
+    }
+
+
+
+    public Araba(){
+    }
+
+
 
 
     public String toString() {
-
-            return "Araba Ozellikleri " +
-                    "\nmarka : " + marka +
-                    "\nmodel : " + model +
-                    "\nyakit : " + yakit +
-                    "\nyil : " + yil +
-                    "\nfiyat : " + fiyat;
-
-        }
-
+        return "Araba Ozellikleri " +
+                "\nmarka : " + marka +
+                "\nmodel : " + model +
+                "\nyakit : " + yakit +
+                "\nyil : " + yil +
+                "\nfiyat : " + fiyat ;
+    }
 
     }
