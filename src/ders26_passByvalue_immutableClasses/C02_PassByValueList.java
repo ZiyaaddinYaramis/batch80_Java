2c287a363969d3f973a868649d6489cfecf0e875
Jr. Full Stack Java Developper <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2022-10-29 11:49:40 +0300

    29.10.22

---
+++ src/ders26_passByvalue_immutableClasses/C02_PassByValueList.java
@@ -0,0 +1,19 @@
+package ders26_passByvalue_immutableClasses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C02_PassByValueList {
+    public static void main(String[] args) {
+
+
+        // bir method olusturalim ve method'da list'deki sayilari 5 artiralim
+        // ve method'da listenin son halini yazdiralim
+
+        List<Integer> sayilar=new ArrayList<>();
+        sayilar.add(10);
+        sayilar.add(20);
+        sayilar.add(30);
+
+    }
+}
