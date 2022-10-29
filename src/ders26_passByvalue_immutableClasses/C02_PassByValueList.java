3160eae34ed85ebb81b630374527cca8f2075f8f
Jr. Full Stack Java Developper <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2022-10-29 22:07:05 +0300

    29.10.22

--- src/ders26_passByvalue_immutableClasses/C02_PassByValueList.java
+++ src/ders26_passByvalue_immutableClasses/C02_PassByValueList.java
@@ -7,14 +7,17 @@ public class C02_PassByValueList {
     public static void main(String[] args) {
 
 
-        // bir method olusturalim ve method'da list'deki sayilari 5 artiralim
+        // Bir method olusturalim ve method'da list'deki sayilari 5 artiralim
         // ve method'da listenin son halini yazdiralim
 
 
         List<Integer> sayilar=new ArrayList<>();
+
         sayilar.add(10);
         sayilar.add(20);
         sayilar.add(30);
+
+
         elementleri5Artir(sayilar);
         elementleri5Artir(sayilar);
         elementleri5Artir(sayilar);
@@ -27,7 +30,9 @@ public class C02_PassByValueList {
 
 
     public static void elementleri5Artir(List<Integer> sayilar){
+
         for (int i = 0; i < sayilar.size() ; i++) {
+
             sayilar.set(i, sayilar.get(i) +5);
         }
         System.out.println(sayilar);
