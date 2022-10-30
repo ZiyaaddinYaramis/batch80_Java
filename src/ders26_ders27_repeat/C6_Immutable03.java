29e8d5b231155e93d66b636f78bf27909b30cee3
Jr. Full Stack Java Developper <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2022-10-30 14:41:55 +0200

    30.10.22

---
+++ src/ders26_ders27_repeat/C6_Immutable03.java
@@ -0,0 +1,23 @@
+package ders26_ders27_repeat;
+
+public class C6_Immutable03 {
+
+    public static void main(String[] args) {
+
+
+        // atama yapildiginda ne olur ?
+
+        String isim="";
+
+        for (int i = 0; i < 10; i++) {
+
+            isim=isim+i;
+
+            System.out.println(isim);
+        }
+
+        System.out.println("================");
+
+        System.out.println(isim);
+    }
+}
