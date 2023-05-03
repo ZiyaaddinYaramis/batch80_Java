7beae8fc071f97e151baa80172cd149ac58ab353
Jr. Full Stack Java Developper <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2023-05-03 23:01:02 +0300

    03.05

--- src/day02datatypesmethodcreation/MethodCreation01.java
+++ src/day02datatypesmethodcreation/MethodCreation01.java
@@ -1,4 +1,32 @@
 package day02datatypesmethodcreation;
 
 public class MethodCreation01 {
+
+
+
+    public static void main(String[] args) {
+        int sonuc = toplamaTap(3,5);
+        System.out.println("sonuc method'u caslisti " + sonuc);
+
+        long carpma = multiply(5,7);
+        System.out.println("carpma method'u caslisti "+ carpma);
+
+        int carpTopla = ucSayi(3,5,7);
+        System.out.println("ucSayi method'u caslisti "+carpTopla);
+
+    }
+
+    public static int toplamaTap (int a,int b){
+
+        return a+b;
+    }
+
+    protected static long multiply(int a,int b ){
+        return a*b;
+    }
+
+
+    private static int ucSayi(int a,int b,int c){
+        return (a*b) +c;
+    }
 }
