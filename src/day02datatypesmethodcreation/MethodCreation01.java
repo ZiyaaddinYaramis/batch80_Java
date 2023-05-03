f7fedb3879c7853ba975599890ec5c8eda670a82
Jr. Full Stack Java Developper <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2023-05-03 23:19:24 +0300

    3.5.23

--- src/day02datatypesmethodcreation/MethodCreation01.java
+++ src/day02datatypesmethodcreation/MethodCreation01.java
@@ -2,31 +2,58 @@ package day02datatypesmethodcreation;
 
 public class MethodCreation01 {
 
+    /*
+    Java da method nasil olusturulur?
 
+    1) main method un disinda classin icinde olusturulur
+    2) Access Modifier + return type + method ismi + () + {}
+
+    Olusturulun method lar nasil kullanilir?
+    1)Methodu olusturmak methodu calistirmak icin yeterli degildir, Kullanilmak istenen
+      method main method unicinden kullanilir
+    2) method un ismi + () yazin
+        varsa islem yapacaginiz datalari parantezin icine koyun
+        bu islem method call (method cagirma) olarak adlandirilir
+    3) methodun ismi + parametreler ===> method signature
+     */
 
     public static void main(String[] args) {
-        int sonuc = toplamaTap(3,5);
+        int sonuc = toplamaYap(3,5);
         System.out.println("sonuc method'u caslisti " + sonuc);
 
-        long carpma = multiply(5,7);
-        System.out.println("carpma method'u caslisti "+ carpma);
+        long carpmaSonucu = multiply(5,7);
+        System.out.println("carpma method'u caslisti "+ carpmaSonucu);
 
-        int carpTopla = ucSayi(3,5,7);
+        int carpTopla = firstTwoMultiplyThirdAdd(3,5,7);
         System.out.println("ucSayi method'u caslisti "+carpTopla);
+
+        //System.out.println(firstTwoMultiplyThirdAdd(3,5,7));
 
     }
 
-    public static int toplamaTap (int a,int b){
+    //Ornek 1: toplama islemi yapan bir method olusturun ve kullaniniz
+    public static int toplamaYap (int a,int b){
 
         return a+b;
     }
 
+    //Ornek 2 sayiyi carpma islemi yapan bir method olusturun ve kullanin
     protected static long multiply(int a,int b ){
         return a*b;
     }
 
 
-    private static int ucSayi(int a,int b,int c){
+    //Ornek 3 verilen 3 sayidan ilk ikisini carpan ve ucuncu sayi ile sonucu toplayan methodu olusturun ve kullanin
+    private static int firstTwoMultiplyThirdAdd(int a,int b,int c){
         return (a*b) +c;
+
+
+        /*
+
+        HOMEWORK
+        Tekrar01 clası oluştur tüm yapılanları tekrar yap.
+        1)  Dikdörtgenin alanini hesaplayan methodu olusturun ve kullaniniz
+        2) Dikdörtgenin cevresini hesaplayan methodu olusturun ve kullaniniz
+        */
     }
 }
