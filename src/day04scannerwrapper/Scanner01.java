9e474de535874d4ded68b521ada21c2fd07f2628
Jr. Full Stack Java Developper <79063746+ZiyaaddinYaramis@users.noreply.github.com>
2023-05-09 21:17:01 +0300

    09.05

--- src/day04scannerwrapper/Scanner01.java
+++ src/day04scannerwrapper/Scanner01.java
@@ -6,5 +6,22 @@ public class Scanner01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Örnek : Kullanicidan ilk ismini ve soy ismini alip ikisini ayni satirda yazdiriniz
+
+        System.out.println("Lutfen ilk isminizi giriniz");
+
+       // String ilkIsim = scan.nextLine();  nextLine() methodu tum metni aralarindaki bosluklarla beraber alir
+
+        String firstName = scan.next();
+
+        System.out.println("Lutfen soy isminizi giriniz");
+
+        String lastName = scan.next();
+
+
+        System.out.println("ilkIsim + soyIsim = "+ firstName + lastName);
+
+
+
     }
 }
